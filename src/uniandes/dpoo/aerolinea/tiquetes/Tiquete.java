863982939438233e5a36454271b733ad0f969afc
Benjifufu <157720609+Benjifufu@users.noreply.github.com>
2025-03-02 20:08:57 -0500

    Add files via upload

---
+++ src/uniandes/dpoo/aerolinea/tiquetes/Tiquete.java
@@ -0,0 +1,46 @@
+package uniandes.dpoo.aerolinea.tiquetes;
+
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
+import uniandes.dpoo.aerolinea.modelo.cliente.Cliente;
+
+public class Tiquete {
+
+	private String codigo;
+	private int tarifa;
+	private boolean usado;
+	private Vuelo vuelo;
+	private Cliente cliente;
+	
+	public Tiquete(String codigo, Vuelo vuelo, Cliente cliente, int tarifa) {
+		this.codigo = codigo;
+        this.vuelo = vuelo;
+        this.cliente = cliente;
+        this.tarifa = tarifa;
+        this.usado = false;
+	}
+	
+	public String getCodigo() {
+        return codigo;
+    }
+
+    public int getTarifa() {
+        return tarifa;
+    }
+
+    public boolean esUsado() {
+        return usado;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void marcarComoUsado() {
+        this.usado = true;
+    }
+
+}
